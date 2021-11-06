fc80258210d408f9af96ddfa4441c436c10ba9e9
Ignacio Barroso <ignacio_barroso@outlook.com>
2021-11-06 10:56:04 -0300

    Project Setup

---
+++ app.py
@@ -0,0 +1,16 @@
+'''
+Tweets
+/ -> Show all tweets
+/post -> Create a new tweet
+/tweets/<tweet_id> -> Show a specific tweet
+/tweets/<tweet_id>/delete -> Delete a specific tweet
+/tweets/<tweet_id>/update -> Update a specific tweet
+
+Users
+/signup -> Create a new user
+/login -> Log in an existing user
+/users -> Show all users
+/users/<user_id> -> Show a specific user
+/users/<user_id>/delete -> Delete a specific user
+/users/<user_id>/update -> Update a specific user
+'''
